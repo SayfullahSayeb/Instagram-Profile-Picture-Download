4ca1d6c0262909155178b1d202c9a286494b91ba
Sayfullah Sayeb <sayfullah.sayeb@gmail.com>
2025-10-23 23:51:44 +0600

    Update author and add open-in-new-tab feature

--- main.user.js
+++ main.user.js
@@ -3,7 +3,7 @@
 // @namespace          https://github.com/SayfullahSayeb/Instagram-Profile-Picture-Download/
 // @version            1.0.0
 // @description        Download Instagram profile pictures in full resolution.
-// @author             SN-Koarashi (5026)
+// @author             Sayfullah Sayeb
 // @match              https://*.instagram.com/*
 // @grant              GM_xmlhttpRequest
 // @grant              GM_addStyle
@@ -22,7 +22,8 @@
 
     // SVG icons
     const SVG = {
-        DOWNLOAD: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z"/></g></svg>'
+        DOWNLOAD: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24"/></g><g><path d="M18,15v3H6v-3H4v3c0,1.1,0.9,2,2,2h12c1.1,0,2-0.9,2-2v-3H18z M17,11l-1.41-1.41L13,12.17V4h-2v8.17L8.41,9.59L7,11l5,5 L17,11z"/></g></svg>',
+        OPEN: '<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 24 24" fill="#000000"><path d="M14 3v2h3.59L10 12.59l1.41 1.41L19 6.41V10h2V3h-7z"/><path d="M5 5v14h14v-7h-2v5H7V7h5V5H5z"/></svg>'
     };
 
     // Add CSS styles
@@ -42,6 +43,26 @@
             background: #eee;
         }
         .IG_DWPROFILE svg {
+            display: block;
+            width: 20px;
+            height: 20px;
+        }
+        /* New open-in-new-tab button, placed left of download */
+        .IG_OPENPROFILE {
+            position: absolute;
+            right: 32px;
+            bottom: 5px;
+            background: #fff;
+            border-radius: 4px;
+            padding: 2px;
+            cursor: pointer;
+            z-index: 999;
+            filter: drop-shadow(0px 0px 2px rgba(0,0,0,0.5));
+        }
+        .IG_OPENPROFILE:hover {
+            background: #eee;
+        }
+        .IG_OPENPROFILE svg {
             display: block;
             width: 20px;
             height: 20px;
@@ -193,15 +214,19 @@
                 });
             }
         } else {
-            if (!$('.IG_DWPROFILE').length) {
+            if (!$('.IG_DWPROFILE').length && !$('.IG_OPENPROFILE').length) {
                 let profileTimer = setInterval(() => {
-                    if ($('.IG_DWPROFILE').length) {
+                    if ($('.IG_DWPROFILE').length || $('.IG_OPENPROFILE').length) {
                         clearInterval(profileTimer);
                         return;
                     }
 
+                    // Insert both buttons: open (left) and download (right)
+                    $('header > *[class]:first-child > *[class] img[alt][draggable]').parent().parent().append(`<div title="Open Profile Picture in New Tab" class="IG_OPENPROFILE">${SVG.OPEN}</div>`);
                     $('header > *[class]:first-child > *[class] img[alt][draggable]').parent().parent().append(`<div title="Download Profile Picture" class="IG_DWPROFILE">${SVG.DOWNLOAD}</div>`);
                     $('header > *[class]:first-child > *[class] img[alt][draggable]').parent().parent().css('position', 'relative');
+
+                    $('header > *[class]:first-child > *[class] img[alt]:not([draggable])').parent().parent().parent().append(`<div title="Open Profile Picture in New Tab" class="IG_OPENPROFILE">${SVG.OPEN}</div>`);
                     $('header > *[class]:first-child > *[class] img[alt]:not([draggable])').parent().parent().parent().append(`<div title="Download Profile Picture" class="IG_DWPROFILE">${SVG.DOWNLOAD}</div>`);
                     $('header > *[class]:first-child > *[class] img[alt]:not([draggable])').parent().parent().parent().css('position', 'relative');
                 }, 150);
@@ -241,6 +266,38 @@
         $('body').on('click', '.IG_DWPROFILE', function(e) {
             e.stopPropagation();
             onProfileAvatar(true);
+        });
+
+        // Click event for open-in-new-tab button
+        $('body').on('click', '.IG_OPENPROFILE', async function(e) {
+            e.stopPropagation();
+            try {
+                let username = location.pathname.replaceAll(/(reels|tagged)\/$/ig, '').split('/').filter(s => s.length > 0).at(-1);
+                let userInfo = await getUserId(username);
+                try {
+                    let dataURL = await getUserHighSizeProfile(userInfo.user.pk);
+                    window.open(dataURL, '_blank');
+                } catch (err) {
+                    console.warn('Failed to get HD profile picture for opening, falling back', err);
+                    let fallbackURL = userInfo.user.profile_pic_url;
+                    if (fallbackURL) {
+                        window.open(fallbackURL, '_blank');
+                    } else {
+                        GM_notification({
+                            text: 'No profile picture available to open.',
+                            title: 'Open Failed',
+                            timeout: 3000
+                        });
+                    }
+                }
+            } catch (err) {
+                console.error('Failed to open profile picture:', err);
+                GM_notification({
+                    text: 'Failed to open profile picture. Please try again.',
+                    title: 'Open Failed',
+                    timeout: 3000
+                });
+            }
         });
     });
 
